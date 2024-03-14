e86edd385f28a70af5d450b26d1f314ede19607c
Trisha_Sasikumar <trishassaikumar26@gmail.com>
2024-03-14 15:23:39 +0530

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '6b1b0a83-1018-4023-b1e1-61dab06d66b7';
+export const MODAL_CLOSE_SEC = 2.5;
